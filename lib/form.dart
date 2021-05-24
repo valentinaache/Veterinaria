110bc42c0f4bbd0e5acf7376f9f603057e911733
Wilson Andrés Camargo <49167124+wilsoncamgo@users.noreply.github.com>
2021-05-23 20:00:39 -0500

    Update form.dart

--- lib/form.dart
+++ lib/form.dart
@@ -53,77 +53,38 @@ class MyCustomFormState extends State<MyCustomForm> {
       ),
     );
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My Form"),
-      ),
-      body: Container(
-          decoration: BoxDecoration(color: Colors.blue),
-          padding: EdgeInsets.all(20.0),
-          width: double.infinity,
-          child: Container(
-            padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-            decoration: BoxDecoration(color: Colors.white),
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(
-                  "Login Form",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w700),
-                ),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      MyFormTextField(
-                        isObscure: false,
-                        decoration: InputDecoration(
-                          labelText: "EmailAddress",
-                          hintText: "me@abc.com",
-                        ),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter an email address';
-                          } else if (!_emailRegExp.hasMatch(value)) {
-                            return 'Invalid email address!';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                          model.emailAddress = value;
-                        },
-                      ),
-                      MyFormTextField(
-                        isObscure: true,
-                        decoration: InputDecoration(
-                            labelText: "Password", hintText: "my password"),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter a password';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                          model.password = value;
-                        },
-                      ),
-                      FormSubmitButton(
-                        onPressed: () {
-                          // Validate returns true if the form is valid, otherwise false.
-                          if (_formKey.currentState.validate()) {
-                            _formKey.currentState.save();
-                            print(model);
-
-                            Scaffold.of(_formKey.currentContext).showSnackBar(
-                                SnackBar(content: Text('Processing Data')));
-                          }
-                        },
-                      ),
-                    ],
+                SizedBox(
+                  height: 155.0,
+                  child: Image.asset(
+                    "assets/logo.png",
+                    fit: BoxFit.contain,
                   ),
-                )
+                ),
+                SizedBox(height: 45.0),
+                emailField,
+                SizedBox(height: 25.0),
+                passwordField,
+                SizedBox(
+                  height: 35.0,
+                ),
+                loginButon,
+                SizedBox(
+                  height: 15.0,
+                ),
               ],
             ),
-          )),
+          ),
+        ),
+      )
     );
   }
 }
